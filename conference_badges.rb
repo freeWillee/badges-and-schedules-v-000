e97e8557dadd2a924f995f0e31d7cf7e6dde3cb0
Will Lee <will.i.lee@outlook.com>
2018-06-01 16:01:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,8 +18,8 @@ end
 
 
 def assign_rooms(attendees)
+# Is there a more efficient way to write the assign_rooms method?
 
-# Is there a more efficient way to write the assign_rooms method?  
   room_list = []
   attendees.each_with_index{ |item, index|
     room_list << "Hello, #{item}! You'll be assigned to room #{index + 1}!"
