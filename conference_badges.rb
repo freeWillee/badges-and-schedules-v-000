0a899c10896a6c487c777568cb9d4c6583fe686b
Will Lee <will.i.lee@outlook.com>
2018-06-01 15:50:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  i = 0
+  message_array = []
+  while i < name_array.length
+    message_array << badge_maker(name_array[i])
+    i += 1
+  end
+
+  return message_array
+end
+
+def assign_rooms(attendees)
+  room_list = []
+  attendees.each_with_index{ |item, index|
+    room_list[item] = index
+    binding.pry    
+  }
+  room_list << "Hello, #{}! You'll be assigned to room #{i}!"
+  i += 1
+end
